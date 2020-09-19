409ab050c1d5290c5b5bf2164068f9855efa0bda
Simran Raj <simran.raj@Triconinfotech.com>
2020-09-19 17:19:52 +0530

    rendering image changes

--- chatbot-frontend/src/components/chatWidget.js
+++ chatbot-frontend/src/components/chatWidget.js
@@ -1,36 +1,46 @@
-import React, {useState} from 'react'
-import {Launcher} from 'react-chat-window'
-import {sendMessage, postFormData} from '../network/message'
+import React, { useState } from 'react'
+import { Launcher } from 'react-chat-window'
+import { sendMessage, postFormData } from '../network/message'
 
 const sender = Date.now().toString()
 
-const Demo = () =>{
+const Demo = () => {
   const [messageList, setMessageList] = useState([])
-  
-  const onMessageWasSent = async(message) => {
+
+  const onMessageWasSent = async (message) => {
     console.log(message)
     messageList.push(message)
-    const data={
-      "sender": sender ,
+    const data = {
+      "sender": sender,
       "message": message.data.text
     }
     sendMessageToServer(data)
   }
- 
-  const sendMessageToServer= async(data) => {
+
+
+  const sendMessageToServer = async (data) => {
     console.log(JSON.stringify(data))
-    const response = await sendMessage(data)
-    let responseMessage= {
+    // const response = await sendMessage(data)
+    let responseMessage = {
       author: 'them',
-      type: 'text',
+      type: 'file',
       data: {
-        text: response
+        url: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png',
+        fileName: 'random',
       }
     }
     setMessageList([...messageList, responseMessage])
+    createUI('https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png');
   }
-  
-  const onFilesSelected = async(fileList) =>{
+
+  const createUI = (href) => {
+    var els = document.querySelectorAll(`a[href="${href}"]`);
+    for (let i = 0; i < els.length; i++) {
+      els[i].children[0].innerHTML = `<img src="${href}">`
+    }
+  }
+
+  const onFilesSelected = async (fileList) => {
     console.log(fileList[0])
     const formData = new FormData();
     formData.append("message", fileList[0]);
@@ -38,18 +48,18 @@ const Demo = () =>{
     postFormData(formData)
 
   }
-    return (<div>
-      <Launcher
-        agentProfile={{
-          teamName: 'Bearopedia',
-          imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
-        }}
-        onMessageWasSent={onMessageWasSent}
-        messageList={messageList}
-        onFilesSelected= {onFilesSelected}
-      />
-    </div>)
-  }
+  return (<div>
+    <Launcher
+      agentProfile={{
+        teamName: 'Bearopedia',
+        imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+      }}
+      onMessageWasSent={onMessageWasSent}
+      messageList={messageList}
+      onFilesSelected={onFilesSelected}
+    />
+  </div>)
+}
 
 
 export default Demo
