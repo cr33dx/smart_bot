56c2579fda6b6daae6a5d2339ef0acfac89fbd88
lakshyak789 <lakshyak789@gmail.com>
2020-09-19 14:03:13 +0530

    chatbot front end

---
+++ chatbot-frontend/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import { Widget } from 'react-chat-widget';
+import Widget from '../components/chatWidget'
+ 
+import 'react-chat-widget/lib/styles.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <Widget />
+    </div>
+  );
+}
+
+
+export default App
